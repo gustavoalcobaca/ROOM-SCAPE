26347fb0748f3e7f9b90d9c704ded7e1c210ddd3
Jonas Henrique Duarte Silva <duartesilvjonashenry@gmail.com>
2026-08-08 17:39:37 -0300

    O GOT . GAROTO DE PROGRAMA

---
+++ scape-room-(4.6)/Script/BancoMaterias.gd
@@ -0,0 +1,82 @@
+extends Node
+
+var fisica = {
+
+	"1º Ano": {
+		"progresso": 0,
+		"concluidos": [],
+		"proximo": "Em desenvolvimento...",
+		"pendentes": []
+	},
+
+	"2º Ano": {
+		"progresso": 0,
+		"concluidos": [],
+		"proximo": "Em desenvolvimento...",
+		"pendentes": []
+	},
+
+	"3º Ano": {
+		"progresso": 83,
+
+		"concluidos": [
+			"Cinemática",
+			"Leis de Newton"
+		],
+
+		"proximo": "Gravitação Universal",
+
+		"pendentes": [
+			"Ondulatória",
+			"Óptica"
+		]
+	}
+}
+
+var quimica = {
+
+	"1º Ano": {
+		"progresso": 0,
+		"concluidos": [],
+		"proximo": "Em desenvolvimento...",
+		"pendentes": []
+	},
+
+	"2º Ano": {
+		"progresso": 0,
+		"concluidos": [],
+		"proximo": "Em desenvolvimento...",
+		"pendentes": []
+	},
+
+	"3º Ano": {
+		"progresso": 0,
+		"concluidos": [],
+		"proximo": "Em desenvolvimento...",
+		"pendentes": []
+	}
+}
+
+var biologia = {
+
+	"1º Ano": {
+		"progresso": 0,
+		"concluidos": [],
+		"proximo": "Em desenvolvimento...",
+		"pendentes": []
+	},
+
+	"2º Ano": {
+		"progresso": 0,
+		"concluidos": [],
+		"proximo": "Em desenvolvimento...",
+		"pendentes": []
+	},
+
+	"3º Ano": {
+		"progresso": 0,
+		"concluidos": [],
+		"proximo": "Em desenvolvimento...",
+		"pendentes": []
+	}
+}
